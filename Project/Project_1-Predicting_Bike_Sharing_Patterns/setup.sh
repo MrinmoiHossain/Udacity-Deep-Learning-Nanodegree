9cb9e790678fde1d5a24e311344cb53433cefe3b
MrinmoiHossain <mokit.aust@gmail.com>
2019-10-12 23:16:34 +0600

    Predicting Bike Sharing Patterns Project
    In requirements file, all necessary libary are included. "my_answer.py"
    is the main file for this project. In this file, Neural Network model is
    implemented with train, forward_pass_train, backpropagation,
    update_weights and run function. And, "Your_firs_neural_network.ipynb"
    is another main file for this project where data are loaded and model
    are trained.
    
    Solve: NeuralNetwork Model

---
+++ Project/Project_1-Predicting_Bike_Sharing_Patterns/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Download your JWT by clicking here:"
+echo "https://project-assistant.udacity.com/auth_tokens/new"
+echo
+echo "Once you've downloaded your JWT, copy the contents into the editor tab titled "jwt" (The file is: /home/workspace/first-neural-network/jwt)"
+echo "After that step, you can run the project assistant. To do that, first go to the first-neural-network directory ('cd first-neural-network') and then submit your project with : udacity submit first-neural-network"
